3f1c0be5bb2067f8fe510cc9a20de1f6e0afcfa0
Aleksey Istomin <auistomin@yandex.ru>
2021-03-11 20:38:27 +1100

    Update

---
+++ tours/views.py
@@ -0,0 +1,22 @@
+# from django.http import HttpResponseNotFound
+from django.shortcuts import render
+
+
+def main_view(request):
+    return render(request, "index.html")
+
+
+def departure_view(request, departure):
+    return render(request, "departure.html")
+
+
+def tour_view(request, id):
+    return render(request, "tour.html")
+
+
+def custom_handler404(request, exception):
+    return render(request, "404.html")
+
+
+def custom_handler500(request):
+    return render(request, "500.html")
