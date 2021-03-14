7f0caca35e78782b3f3ea959ab377c60ad1e5910
Aleksey Istomin <auistomin@yandex.ru>
2021-03-14 20:02:57 +1100

    Update

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from os import path as os_path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os_path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -22,12 +22,12 @@ from tours.views import tour_view
 from tours.views import custom_handler404, custom_handler500
 
 
-handler404 = custom_handler404
-handler500 = custom_handler500
-
 urlpatterns = [
     path('', main_view, name='main'),
     path('departure/<str:departure>/', departure_view, name='departure'),
-    path('tour/<int:id>/', tour_view, name='tour'),
+    path('tour/<int:tour_id>/', tour_view, name='tour'),
     path('admin/', admin.site.urls),
 ]
+
+handler404 = custom_handler404
+handler500 = custom_handler500
